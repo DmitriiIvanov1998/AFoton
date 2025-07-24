faa0fed0b4e71ffc324431a633ee3f231904389a
DmitriiIvanov1998 <64757380+DmitriiIvanov1998@users.noreply.github.com>
2025-07-24 20:23:03 +0300

    Create Task_1.cs

---
+++ Task_1.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Text;
+
+// Создаём основной класс
+class Program
+
+{
+    // Создаем процедуру, не требующую конкретного объекта
+    static void Main(string[] args)
+
+    {
+        // Задаём переменную
+        int number = 100;
+        
+        // Делаем так, чтобы работали русские буквы
+        Console.OutputEncoding = Encoding.UTF8;
+
+        // Пишется в консоли то, что в кавычках
+        // $ делает из строки умну/ строк
+        Console.Write($"Извлечение квадратного корня из {number} в столбик");
+        
+        Console.WriteLine($" = {Math.Sqrt(number)}");
+        ;
+
+    }
+
+    static string ManualSqrt(int number)
+
+    {
+        // переводим число в строку
+        string numStr = number.ToString();
+        
+        // находим длину строки
+        int len = numStr.Length;
+
+        // находим максимальное кол-во групп
+        int groups = (len + 1) / 2;
+
+        // создаём пустой список ( массив ) 
+        int[] pairs = new int[groups];
+
+
+        // запускаем цикл
+        for (int i = 0; i < len; i++)
+
+        {
+            if (len % 2 == 1 && i == 0)
+            { // в случае, если длина не чётная и мы в начале цикла
+            // то в первый элемент массива кладём первую цифру числа
+                pairs[0] = int.Parse(numStr.Substring(0, 1));
+            }
+            else
+            {// иначе 
+            
+                int pairIndex = 0;
+                if (len % 2 == 1)  
+                    {
+                    pairIndex = i / 2 + 1 ;
+                        
+                    } 
+                else  
+                    {
+                    pairIndex = i / 2 ;
+                        
+                    }
+                pairs[pairIndex] *= 10;
+                pairs[pairIndex] += (numStr[i] - '0');
+            }
+
+        }
+
+        string result = "";
+
+        int remainder = 0;
+
+        for (int i = 0; i < groups; i++)
+
+        {
+
+            int current = remainder * 100 + pairs[i];
+
+            int x = 0;
+
+            for (int j = 1; j <= 9; j++)
+
+            {
+
+                int trial = (int.Parse(result + j.ToString()) * j);
+
+                if (trial <= current)
+
+                {
+
+                    x = j;
+
+                }
+
+                else
+
+                {
+
+                    break;
+
+                }
+
+            }
+
+            result += x.ToString();
+
+            remainder = current - (int.Parse(result) * x);
+
+        }
+
+        return result;
+
+    }
+
+}
